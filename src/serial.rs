5bd837b34beda3a3c6f856a110048970dd0f8e2f
Aaditya Aren <aadityaaren01@Aadityas-MacBook-Air.local>
2024-04-18 19:14:14 +0530

    Add A Freestanding Rust Binary, A Minimal Rust Kernel, VGA Text MOde and Testing completing Bare Bones for the project

--- src/serial.rs
+++ src/serial.rs
@@ -25,7 +25,7 @@ macro_rules! serial_print {
 }
 
 /// Prints to the host through the serial interface, appending a newline.
-#[macro_export]
+#[macro_export] // exporting function to root(main file)
 macro_rules! serial_println {
     () => ($crate::serial_print!("\n"));
     ($fmt:expr) => ($crate::serial_print!(concat!($fmt, "\n")));
